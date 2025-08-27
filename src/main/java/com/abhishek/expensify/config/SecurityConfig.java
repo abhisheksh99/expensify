29ff2ba1dd79ba5c4c226b40d81fea453388e366
abhisheksh99 <abhishekleo1234@gmail.com>
2025-08-26 23:03:55 -0400

    Minor change in security config

--- src/main/java/com/abhishek/expensify/config/SecurityConfig.java
+++ src/main/java/com/abhishek/expensify/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET","POST","DELETE","OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET","POST","DELETE","OPTIONS","PUT","PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type","Accept"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
